c378574afd53f319f83beaef395e61d6158f3630
Matt Jones <matt@tother.me.uk>
2015-09-02 14:55:45 +0100

    Initial checkin.

---
+++ src/Jonesmatt.Ninject.Extensions.Azure/Jonesmatt.Ninject.Extensions.AzureWorker/NinjectRoleEntryPoint.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using Ninject;
+
+namespace Jonesmatt.Ninject.Extensions.AzureWorker
+{
+    public abstract class NinjectRoleEntryPoint : RoleEntryPoint
+    {
+        protected IKernel Kernel { get; set; }
+
+        public sealed override bool OnStart()
+        {
+            Kernel = CreateKernel();
+            Kernel.Inject(this);
+            return OnRoleStarted();
+        }
+
+        public abstract override void Run();
+
+        public sealed override void OnStop()
+        {
+            OnRoleStopping();
+
+            if (Kernel != null)
+            {
+                Kernel.Dispose();
+                Kernel = null;
+            }
+
+            OnRoleStopped();
+        }
+
+        protected abstract IKernel CreateKernel();
+
+        protected virtual bool OnRoleStarted()
+        {
+            return true;
+        }
+
+        protected virtual void OnRoleStopping()
+        {
+        }
+
+        protected virtual void OnRoleStopped()
+        {
+        }
+    }
+}
